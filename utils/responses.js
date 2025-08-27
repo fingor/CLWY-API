f6c99860514b96c33e438828815e1f92ff6ccc9b
HAOCHENG <136605477@qq.com>
2025-08-27 09:59:10 +0800

    $http返回errorType

--- utils/responses.js
+++ utils/responses.js
@@ -1,5 +1,5 @@
 const createError = require("http-errors");
-const multer = require('multer');
+const multer = require("multer");
 
 /**
  * 请求成功
@@ -25,6 +25,7 @@ function failure(res, error) {
   // 默认响应为 500，服务器错误
   let statusCode = 500;
   let errors = "服务器错误";
+  let errorType = "SERVER_ERROR"; // 声明 errorType 变量
 
   if (error.name === "SequelizeValidationError") {
     // Sequelize 验证错误
@@ -37,6 +38,7 @@ function failure(res, error) {
     // Token 验证错误
     statusCode = 401;
     errors = "您提交的token错误或已过期。";
+    errorType = "TOKEN_EXPIRED";
   } else if (error instanceof createError.HttpError) {
     // http-errors 库创建的错误
     statusCode = error.status;
@@ -55,64 +57,9 @@ function failure(res, error) {
     status: false,
     message: `请求失败: ${error.name}`,
     errors: Array.isArray(errors) ? errors : [errors],
+    errorType: errorType,
   });
 }
-// function failure(res, error) {
-//   console.log('error',error);
-
-//   if (error.name === "SequelizeValidationError") {
-//     const errors = error.errors.map((e) => e.message);
-//     return res.status(400).json({
-//       status: false,
-//       message: "请求参数错误",
-//       errors,
-//     });
-//   }
-
-//   if (error.name === "NotFound") {
-//     return res.status(404).json({
-//       status: false,
-//       message: "资源不存在",
-//       errors: [error.message],
-//     });
-//   }
-//   if (error.name === "BadRequest") {
-//     return res.status(400).json({
-//       status: false,
-//       message: "请求参数错误",
-//       errors: [error.message],
-//     });
-//   }
-
-//   if (error.name === "Unauthorized") {
-//     return res.status(401).json({
-//       status: false,
-//       message: "认证失败",
-//       errors: [error.message],
-//     });
-//   }
-//   if (error.name === "JsonWebTokenError") {
-//     return res.status(401).json({
-//       status: false,
-//       message: "认证失败",
-//       errors: ["您提交的 token 错误。"],
-//     });
-//   }
-
-//   if (error.name === "TokenExpiredError") {
-//     return res.status(401).json({
-//       status: false,
-//       message: "认证失败",
-//       errors: ["您的 token 已过期。"],
-//     });
-//   }
-
-//   res.status(500).json({
-//     status: false,
-//     message: "服务器错误",
-//     errors: [error.message],
-//   });
-// }
 
 module.exports = {
   success,
